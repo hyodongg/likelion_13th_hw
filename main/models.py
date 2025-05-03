43763d130fcf6e0d3e55f36c2c157b1a93b098ed
HyoDong Jo <whgyehdjhd@naver.com>
2025-05-04 02:37:01 +0900

    4주차 과제

--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title=models.CharField(max_length=30)
+    writer=models.CharField(max_length=20)
+    content=models.TextField()
+    created_time=models.DateTimeField()
+    updated_time=models.DateTimeField()
+    feeling=models.CharField(max_length=10)
+    image = models.ImageField(upload_to="post/",blank=True,null=True)
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.content[:3]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.utils import timezone
+from .models import *
 
 # Create your views here.
 
@@ -21,4 +23,47 @@ def mainpage(request):
 
 
 def secondpage(request):
-    return render(request, "main/secondpage.html")
+    posts=Post.objects.all()
+    return render(request, "main/secondpage.html",{'posts':posts})
+
+def new_post(request):
+    return render(request,'main/hdpost.html')
+
+def detail(request,id):
+    post = get_object_or_404(Post, pk=id)
+    return render(request, 'main/detail.html',{'post': post})
+
+def create(request):
+    new_post = Post()
+
+    new_post.title = request.POST['title']
+    new_post.writer = request.POST['writer']
+    new_post.content = request.POST['content']
+    new_post.feeling = request.POST['feeling']
+    new_post.created_time = timezone.now()
+    new_post.updated_time = timezone.now()
+    new_post.image=request.FILES.get('image')
+
+    new_post.save()
+
+    return redirect('main:detail',new_post.id)
+
+def edit(request,id):
+    edit_post=Post.objects.get(pk=id)
+    return render(request,'main/edit.html',{"post":edit_post})
+
+def update(request,id):
+    update_post=Post.objects.get(pk=id)
+    update_post.title=request.POST['title']
+    update_post.writer=request.POST['writer']
+    update_post.content=request.POST['content']
+    update_post.feeling=request.POST['feeling']
+    update_post.updated_time=timezone.now()
+    update_post.image=request.FILES.get('image')
+    update_post.save()
+    return redirect('main:detail',update_post.id)
+
+def delete(request,id):
+    delete_post=Post.objects.get(pk=id)
+    delete_post.delete()
+    return redirect('main:secondpage')
