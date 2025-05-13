1cbf341c98825443d0f090643482e47f138bc0cb
HyoDong Jo <whgyehdjhd@naver.com>
2025-05-13 19:13:42 +0900

    5주차 과제

--- project/settings.py
+++ project/settings.py
@@ -41,7 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'main'
+    'main',
+    'accounts',
+    'users',
 ]
 
 MIDDLEWARE = [
---
+++ users/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render,get_object_or_404
+from django.contrib import auth
+from django.contrib.auth.models import User
+from main.models import Post
+
+
+# Create your views here.
+def mypage(request,id):
+    user=get_object_or_404(User,pk=id)
+    posts=Post.objects.filter(writer=user)
+    context={
+        'user':user,
+        'posts' :posts,
+    }
+    return render(request,'users/mypage.html',context)
